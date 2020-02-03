1473e805df2960f8ed83b592de6649a2e8dc679a
Pranav <pranavgupta000@gmail.com>
2020-02-03 13:46:02 +0530

    added greaterthanequal lessthanequalto funcitons

--- BTree.cpp
+++ BTree.cpp
@@ -177,26 +177,19 @@ public:
         std::cout << std::endl;
     }
 
-    void iterateLeftLeaf(Node* node, int startIndex){
-        while(node!=nullptr){
-            for(int i=startIndex;i>=0;i--){
-                printf("%d ", node->keys[i]);
-            }
-            node = node->leftSibling_;
-            if(node)
-            startIndex = node->size-1;
-        }
+    void greaterThanEquals(const key_t& key) {
+        auto searchRes = search(key);
+        iterateRightLeaf(searchRes.node, searchRes.index);
+        printf("\n");
     }
 
-    void iterateRightLeaf(Node* node, int startIndex){
-        while(node!=nullptr){
-            for(int i=startIndex;i<node->size;i++){
-                printf("%d ", node->keys[i]);
-            }
-            node = node->rightSibling_;
-            startIndex=0;
-        }
+    void smallerThanEquals(const key_t& key) {
+        auto searchRes = search(key);
+        iterateLeftLeaf(searchRes.node, searchRes.index);
+        printf("\n");
     }
+
+
 
 private:
     // MARK:- HELPER FUNCTIONS
@@ -452,6 +445,28 @@ private:
             parent = child;
         }
     }
+
+    void iterateLeftLeaf(Node* node, int startIndex){
+        while(node!=nullptr){
+            for(int i=startIndex;i>=0;i--){
+                printf("%d ", node->keys[i]);
+            }
+            node = node->leftSibling_;
+            if(node)
+            startIndex = node->size-1;
+        }
+    }
+
+    void iterateRightLeaf(Node* node, int startIndex){
+        while(node!=nullptr){
+            for(int i=startIndex;i<node->size;i++){
+                printf("%d ", node->keys[i]);
+            }
+            node = node->rightSibling_;
+            startIndex=0;
+        }
+    }
+
 };
 
 void BPTreeTest(){
@@ -483,26 +498,32 @@ void BPTreeTest(){
 
     std::cout << "Insert done" << std::endl;
 
-    // std::cout << bt.remove(71) << std::endl;
-    // bt.bfsTraverse();
-    // std::cout << bt.remove(21) << std::endl;
-    // bt.bfsTraverse();
-    // std::cout << bt.remove(51) << std::endl;
-    // bt.bfsTraverse();
-    // std::cout << bt.remove(11) << std::endl;
-    // bt.bfsTraverse();
-    // bt.insert(11);
-    // bt.bfsTraverse();
-    auto searchRes = bt.search(11);
-    std::cout << searchRes.node << std::endl;
-    std::cout << searchRes.index << std::endl;
-    bt.iterateRightLeaf(searchRes.node, searchRes.index);
+     std::cout << bt.remove(71) << std::endl;
+     bt.bfsTraverse();
+     std::cout << bt.remove(21) << std::endl;
+     bt.bfsTraverse();
+     std::cout << bt.remove(51) << std::endl;
+     bt.bfsTraverse();
+     std::cout << bt.remove(11) << std::endl;
+     bt.bfsTraverse();
+     bt.insert(11);
+     bt.bfsTraverse();
+    // auto searchRes = bt.search(11);
+    // std::cout << searchRes.node << std::endl;
+    // std::cout << searchRes.index << std::endl;
+    // bt.iterateRightLeaf(searchRes.node, searchRes.index);
 
-    // bt.bfsTraverse();
-    // std::cout << bt.remove(10) << std::endl;
-    // bt.bfsTraverse();
-    // std::cout << bt.remove(5) << std::endl;
-    // bt.bfsTraverse();
+    bt.smallerThanEquals(11);
+    bt.greaterThanEquals(11);
+
+     bt.bfsTraverse();
+     std::cout << bt.remove(10) << std::endl;
+     bt.bfsTraverse();
+     std::cout << bt.remove(5) << std::endl;
+     bt.bfsTraverse();
+
+    bt.smallerThanEquals(11);
+    bt.greaterThanEquals(17);
 }
 
 int main(){
